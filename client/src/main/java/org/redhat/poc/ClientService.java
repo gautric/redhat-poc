fc9925e201f7de44a1d9cd2a038a12b6b307a4d3
Charles Moulliard <cmoulliard@fusesource.com>
2012-12-13 18:49:09 +0100

    Update client code to contact backend-ws service

--- client/src/main/java/org/redhat/poc/ClientService.java
+++ client/src/main/java/org/redhat/poc/ClientService.java
@@ -1,15 +1,32 @@
 package org.redhat.poc;
 
+import com.redhat.fusetraining.learning.webservice.Administration;
+import com.redhat.fusetraining.learning.webservice.AdministrationImplService;
+import com.redhat.fusetraining.learning.webservice.Course;
+import com.redhat.fusetraining.learning.webservice.Student;
+
+import java.util.List;
+
 public class ClientService {
 
+    Administration admin;
+    Course course;
 
+    AdministrationImplService adminService;
 
-   public String[] getStudents() {
-       return null;
-   }
+    public ClientService() {
+        AdministrationImplService adminService = new AdministrationImplService();
+        admin = adminService.getAdministrationImplPort();
+        course = new Course();
+        course.setContent("terrific");
+    }
 
-    public String[] getCourses() {
-        return null;
+    public List<Student> getStudents() {
+        return admin.getStudentList(course);
+    }
+
+    public List<Course> getCourses() {
+        return admin.getCourseList();
     }
 
 }
--- client/src/main/java/org/redhat/poc/MainApplication.java
+++ client/src/main/java/org/redhat/poc/MainApplication.java
@@ -1,7 +1,11 @@
 package org.redhat.poc;
 
+import com.redhat.fusetraining.learning.webservice.Course;
+import com.redhat.fusetraining.learning.webservice.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class MainApplication {
 
@@ -15,13 +19,26 @@ public class MainApplication {
 
         // Call getStudents
         logger.info(">> Get Students");
-        clientService.getStudents();
+        List<Student> students = clientService.getStudents();
+        for(Student student : students) {
+            logger.info("Student First Name : " + student.getFirstName());
+            logger.info("Last Name : " + student.getLastName());
+            logger.info("City : " + student.getCity());
+            logger.info("Email : " + student.getEmail());
+            logger.info("Birthday : " + student.getBirthday());
+            logger.info("***************");
+        }
 
         // Call getCourses
-        logger.info(">> Get Students");
-        clientService.getCourses();
-
-
+        logger.info(">> Get Courses");
+        List<Course> courses = clientService.getCourses();
+        for(Course course : courses) {
+            logger.info("Course : " + course.getContent());
+            logger.info("Title : " + course.getTitle());
+            logger.info("Max : " + course.getMaxStudent());
+            logger.info("Exam Date : " + course.getExamDate());
+            logger.info("***************");
+        }
 
     }
 
